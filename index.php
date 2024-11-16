1e4cb950b47bc59ce141d87ed6056040fef6a300
divyansh <work.divyansh7303@gmail.com>
2024-11-16 17:24:38 +0530

    16 nov mid

--- index.php
+++ index.php
@@ -132,6 +132,7 @@
                     <h5 class="modal-title" id="exampleModalLabel">Employee details</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+                
                 <div class="modal-body">
                     <span>name : </span><span class="name_view"></span><br>
                     <span>email : </span><span class="email_view"></span><br>
